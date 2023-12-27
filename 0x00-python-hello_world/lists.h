aa41fc25642993c9394d0f1041786a295be09a59
RyUKEnza <kenza_raj@hotmail.fr>
2023-12-27 01:56:12 +0000

    Python launch

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,20 @@
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @data: integer data
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_node {
+	int data;
+	struct list_node *next;
+} list_node;
+
+/* Function prototypes */
+void print_list(const list_node *head);
+list_node *add_node(int data, list_node *head);
+
+#endif /* LISTS_H */
