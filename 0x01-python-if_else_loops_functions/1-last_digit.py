6dc414879e1199b73cf27145c6ba8a3d01d97b01
RyUKEnza <RyUKEnza@github.com>
2024-02-05 06:18:13 -0800

    Added last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastdigit = int(str(abs(number))[-1])
+
+thestring = "Last digit of {} is {}".format(number, lastdigit)
+
+if lastdigit > 5:
+    print(f"{thestring} and is greater than 5")
+elif lastdigit == 0:
+    print(f"{thestring} and is 0")
+else:
+    print(f"{thestring} and is less than 6 and not 0")
+
