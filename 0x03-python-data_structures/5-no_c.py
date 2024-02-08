a9c279fd560bcf624f203546ac601d17f14a9843
RyUKEnza <RyUKEnza@github.com>
2024-02-08 13:22:41 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    ret = " "
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            ret += my_string[i]
-    return ret
+    str_translate = {ord(i): None for i in 'cC'}
+    new_string = my_string.translate(str_translate)
+    return new_string
