8b29d127c8c3538cbb585ebe5329e095e9e148ed
RyUKEnza <kenza_raj@hotmail.fr>
2024-03-28 17:40:46 -0700

    Added temperature query

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Display average temperature (Fahrenheit) by city, ordered by temperature (descending)
+SELECT city, AVG(temperature * 9/5 + 32) AS average_temperature_fahrenheit
+FROM table_name
+GROUP BY city
+ORDER BY average_temperature_fahrenheit DESC;
+
